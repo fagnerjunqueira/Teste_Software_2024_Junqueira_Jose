ad488d1c525b1021503f00cf18dcf1f021e2acc8
Fagner Junqueira <58519744+fagnerjunqueira@users.noreply.github.com>
2024-08-05 15:08:10 -0300

    Config test

---
+++ projeto/test/test_antigravity.py
@@ -0,0 +1,6 @@
+# Importando a função do módulo my_antigravity
+from my_antigravity import antigravity
+
+# Teste simples para verificar a função some_function
+def test_some_function():
+    assert antigravity.some_function() == 42
